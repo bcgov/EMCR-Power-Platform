f68fd837f8e1b5e790f383c90d9cdf6a96d65f74
EricW <EricW@Quartech.com>
2025-03-13 10:44:49 -0400

    added testing function

--- PowerPlatform/Integration/EEW/MqttSubs/EEWWebApiApp/Services/NrCanMqttBackGroundService.cs
+++ PowerPlatform/Integration/EEW/MqttSubs/EEWWebApiApp/Services/NrCanMqttBackGroundService.cs
@@ -43,7 +43,9 @@ namespace EEWWebApiApp.Services
             try
             {
                 // Start the MQTT client and pass the cancellation token
-                var connectTask = ConnectToNrCan(stoppingToken);
+                //var connectTask = ConnectToNrCan(stoppingToken);
+
+                var connectTask = ConnectToTestMqtt(stoppingToken);
 
                 // Wait for the connection to be established
                 while (!_mqttClient.IsConnected && !stoppingToken.IsCancellationRequested)
@@ -213,6 +215,76 @@ namespace EEWWebApiApp.Services
                                 }
                             }
                         }
+                    }
+                    catch (Exception ex)
+                    {
+                        // Log the exception
+                        Console.WriteLine($"An error occurred: {ex.Message}");
+                        isSubscribed = false; // Reset subscription flag on error
+                        JObject jNRCan = new JObject()
+                        {
+                            ["detail"] = ex.Message,
+                            ["type"] = 717350000
+                        };
+
+                        _ = CreateFailureRecord(jNRCan);
+                    }
+                    finally
+                    {
+                        // Wait for 5 seconds before checking the connection again
+                        await Task.Delay(TimeSpan.FromSeconds(5), cancellationToken);
+                    }
+                }
+            }, cancellationToken);
+        }
+
+        public async Task ConnectToTestMqtt(CancellationToken cancellationToken)
+        {
+            await Task.Run(async () =>
+            {
+                while (!cancellationToken.IsCancellationRequested)
+                {
+                    bool isSubscribed = false;
+
+                    try
+                    {
+                        const string mosquitto_org = @"
+                            -----BEGIN CERTIFICATE-----
+                            MIIEAzCCAuugAwIBAgIUBY1hlCGvdj4NhBXkZ/uLUZNILAwwDQYJKoZIhvcNAQEL
+                            BQAwgZAxCzAJBgNVBAYTAkdCMRcwFQYDVQQIDA5Vbml0ZWQgS2luZ2RvbTEOMAwG
+                            A1UEBwwFRGVyYnkxEjAQBgNVBAoMCU1vc3F1aXR0bzELMAkGA1UECwwCQ0ExFjAU
+                            BgNVBAMMDW1vc3F1aXR0by5vcmcxHzAdBgkqhkiG9w0BCQEWEHJvZ2VyQGF0Y2hv
+                            by5vcmcwHhcNMjAwNjA5MTEwNjM5WhcNMzAwNjA3MTEwNjM5WjCBkDELMAkGA1UE
+                            BhMCR0IxFzAVBgNVBAgMDlVuaXRlZCBLaW5nZG9tMQ4wDAYDVQQHDAVEZXJieTES
+                            MBAGA1UECgwJTW9zcXVpdHRvMQswCQYDVQQLDAJDQTEWMBQGA1UEAwwNbW9zcXVp
+                            dHRvLm9yZzEfMB0GCSqGSIb3DQEJARYQcm9nZXJAYXRjaG9vLm9yZzCCASIwDQYJ
+                            KoZIhvcNAQEBBQADggEPADCCAQoCggEBAME0HKmIzfTOwkKLT3THHe+ObdizamPg
+                            UZmD64Tf3zJdNeYGYn4CEXbyP6fy3tWc8S2boW6dzrH8SdFf9uo320GJA9B7U1FW
+                            Te3xda/Lm3JFfaHjkWw7jBwcauQZjpGINHapHRlpiCZsquAthOgxW9SgDgYlGzEA
+                            s06pkEFiMw+qDfLo/sxFKB6vQlFekMeCymjLCbNwPJyqyhFmPWwio/PDMruBTzPH
+                            3cioBnrJWKXc3OjXdLGFJOfj7pP0j/dr2LH72eSvv3PQQFl90CZPFhrCUcRHSSxo
+                            E6yjGOdnz7f6PveLIB574kQORwt8ePn0yidrTC1ictikED3nHYhMUOUCAwEAAaNT
+                            MFEwHQYDVR0OBBYEFPVV6xBUFPiGKDyo5V3+Hbh4N9YSMB8GA1UdIwQYMBaAFPVV
+                            6xBUFPiGKDyo5V3+Hbh4N9YSMA8GA1UdEwEB/wQFMAMBAf8wDQYJKoZIhvcNAQEL
+                            BQADggEBAGa9kS21N70ThM6/Hj9D7mbVxKLBjVWe2TPsGfbl3rEDfZ+OKRZ2j6AC
+                            6r7jb4TZO3dzF2p6dgbrlU71Y/4K0TdzIjRj3cQ3KSm41JvUQ0hZ/c04iGDg/xWf
+                            +pp58nfPAYwuerruPNWmlStWAXf0UTqRtg4hQDWBuUFDJTuWuuBvEXudz74eh/wK
+                            sMwfu1HFvjy5Z0iMDU8PUDepjVolOCue9ashlS4EB5IECdSR2TItnAIiIwimx839
+                            LdUdRudafMu5T5Xma182OC0/u/xRlEm+tvKGGmfFcN0piqVl8OrSPBgIlb+1IKJE
+                            m/XriWr/Cq4h/JfB7NTsezVslgkBaoU=
+                            -----END CERTIFICATE-----
+                            ";
+
+                        var caChain = new X509Certificate2Collection();
+                        caChain.ImportFromPem(mosquitto_org); // from https://test.mosquitto.org/ssl/mosquitto.org.crt
+
+                        using var mqttClient = _mqttFactory.CreateMqttClient();
+                        var mqttClientOptions = new MqttClientOptionsBuilder().WithTcpServer("test.mosquitto.org", 8883)
+                            .WithTlsOptions(new MqttClientTlsOptionsBuilder().WithTrustChain(caChain).Build())
+                            .Build();
+
+                        var connAck = await mqttClient.ConnectAsync(mqttClientOptions);
+                        Console.WriteLine("Connected to test.moquitto.org:8883 with CaFile mosquitto.org.crt: " + connAck.ResultCode);
                     }
                     catch (Exception ex)
                     {
